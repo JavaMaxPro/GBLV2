85937e5a93a68f2b6fd9b2c38ebbbc8157da7d12
Maksim <mgubkov@gmail>
2021-05-24 22:06:21 +0700

    + interface + Main

--- src/ru,gb,level2/lesson1/lesson1.java
+++ src/ru,gb,level2/lesson1/lesson1.java
@@ -1,5 +1,36 @@
+import entity.Motion;
+import obstacles.Obstacles;
+
 public class lesson1 {
+    public  static  Motion[] motions ;
+    public  static Obstacles [] obstacles;
     public static void main(String[] args) {
-        System.out.println("Hi");
+        boolean yes;
+        System.out.println("Старт");
+        for (Motion motion:motions
+             ) {
+            yes = allObstalces(motion);
+            if(!yes) System.out.println(motion + " выбыл ");
+            else System.out.println(motion + " прошел ");
+
+            
+        }
+    }
+    public void setMotions(Motion... motions) {
+        lesson1.motions = motions;
+    }
+    public void setObstacles(Obstacles... obstacles){
+        lesson1.obstacles =obstacles;
+    }
+
+    public static boolean allObstalces(Motion motion){
+        for (Obstacles obstacle:obstacles
+             ) {
+            if(!obstacle.pastObstacles(motion)){
+                return  false;
+            }
+            System.out.println();
+        }
+        return true;
     }
 }
