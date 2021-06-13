dbb005c91f5452de9b21975778eccd6928d12925
Maksim <mgubkov@gmail>
2021-06-13 20:59:55 +0700

    lesson 7 - server start

---
+++ NetworkChatServer/src/ru/gb/java2/chat/server/chat/ClientHandler.java
@@ -0,0 +1,66 @@
+package ru.gb.java2.chat.server.chat;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler {
+    private MyServer server;
+    private final Socket clientSocket;
+    private DataInputStream inputStream;
+    private DataOutputStream outputStream;
+
+    public ClientHandler(MyServer server, Socket clientSocket) {
+        this.server = server;
+        this.clientSocket = clientSocket;
+    }
+
+    public void handle() throws IOException {
+        inputStream = new DataInputStream(clientSocket.getInputStream());
+        outputStream = new DataOutputStream(clientSocket.getOutputStream());
+        new Thread(() -> {
+            try {
+                readMessages();
+            } catch (IOException e) {
+                System.err.println("Failed to process message from client");
+//                e.printStackTrace();
+            } finally {
+                try {
+                    closeConnection();
+                } catch (IOException e) {
+                    System.err.println("Failed to close connection");
+//                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void closeConnection() throws IOException {
+        clientSocket.close();
+        ;
+    }
+
+    private void readMessages() throws IOException {
+        while (true) {
+            String message = inputStream.readUTF().trim();
+            System.out.println("message " + message);
+
+            if (message.startsWith("/end")) {
+                return;
+            } else {
+                processMessage(message);
+            }
+
+        }
+
+    }
+
+    private void processMessage(String message) throws IOException {
+        server.broadcastMessage(message, this);
+    }
+
+    public void sendMessage(String message) throws IOException {
+        outputStream.writeUTF(message);
+    }
+}
---
+++ NetworkChatServer/src/ru/gb/java2/chat/server/chat/MyServer.java
@@ -0,0 +1,45 @@
+package ru.gb.java2.chat.server.chat;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyServer {
+
+    private final List<ClientHandler> clients = new ArrayList<>();
+
+    public void start(int port) {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server has been started");
+
+            while (true) {
+                waitAndProcessNewClientConnection(serverSocket);
+            }
+
+        } catch (IOException e) {
+            System.err.println("Failed t bind port " + port);
+            e.printStackTrace();
+        }
+    }
+
+    private void waitAndProcessNewClientConnection(ServerSocket serverSocket) throws IOException {
+        System.out.println("Waiting for new client connection");
+        Socket clientSocket = serverSocket.accept();
+        System.out.println("Client connect");
+        ClientHandler clientHandler = new ClientHandler(this, clientSocket);
+        clients.add(clientHandler);
+        clientHandler.handle();
+
+    }
+
+    public void broadcastMessage(String message, ClientHandler sender) throws IOException {
+        for (ClientHandler client : clients) {
+            client.sendMessage(message);
+            if (client != sender) {
+                client.sendMessage(message);
+            }
+        }
+    }
+}
